f695461991fbe3f989f11df3041f2434efe3e9a8
renatowow14 <renatogoomes256@gmail.com>
2021-08-06 19:37:18 -0300

    PGADMIN4 LAPIG

---
+++ docker-postgres/README.md
@@ -0,0 +1,12 @@
+#Container Deployment DOCS
+
+https://www.pgadmin.org/docs/pgadmin4/latest/container_deployment.html
+
+#GIT-HUB of project
+
+https://github.com/postgres/pgadmin4
+
+#Docker Hub PGADMIN4
+
+https://hub.docker.com/r/dpage/pgadmin4
+
---
+++ docker-postgres/entrypoint.sh
@@ -0,0 +1,82 @@
+#!/bin/sh
+
+# Populate config_distro.py. This has some default config, as well as anything
+# provided by the user through the PGADMIN_CONFIG_* environment variables.
+# Only update the file on first launch. The empty file is created during the
+# container build so it can have the required ownership.
+source /pgadmin4/env
+
+if [ `wc -m /pgadmin4/config_distro.py | awk '{ print $1 }'` = "0" ]; then
+    cat << EOF > /pgadmin4/config_distro.py
+CA_FILE = '/etc/ssl/certs/ca-certificates.crt'
+LOG_FILE = '/dev/null'
+HELP_PATH = '../../docs'
+DEFAULT_BINARY_PATHS = {
+        'pg': '/usr/local/pgsql-13',
+        'pg-13': '/usr/local/pgsql-13',
+        'pg-12': '/usr/local/pgsql-12',
+        'pg-11': '/usr/local/pgsql-11',
+        'pg-10': '/usr/local/pgsql-10',
+        'pg-9.6': '/usr/local/pgsql-9.6'
+}
+EOF
+
+    # This is a bit kludgy, but necessary as the container uses BusyBox/ash as
+    # it's shell and not bash which would allow a much cleaner implementation
+    for var in $(env | grep PGADMIN_CONFIG_ | cut -d "=" -f 1); do
+        echo ${var#PGADMIN_CONFIG_} = $(eval "echo \$$var") >> /pgadmin4/config_distro.py
+    done
+fi
+
+if [ ! -f /var/lib/pgadmin/pgadmin4.db ]; then
+    if [ -z "${PGADMIN_DEFAULT_EMAIL}" -o -z "${PGADMIN_DEFAULT_PASSWORD}" ]; then
+        echo 'You need to define the PGADMIN_DEFAULT_EMAIL and PGADMIN_DEFAULT_PASSWORD environment variables.'
+        exit 1
+    fi
+
+    echo ${PGADMIN_DEFAULT_EMAIL} | grep -E "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$" > /dev/null
+    if [ $? -ne 0 ]; then
+        echo "'${PGADMIN_DEFAULT_EMAIL}' does not appear to be a valid email address. Please reset the PGADMIN_DEFAULT_EMAIL environment variable and try again."
+        exit 1
+    fi
+
+    # Set the default username and password in a
+    # backwards compatible way
+    export PGADMIN_SETUP_EMAIL=${PGADMIN_DEFAULT_EMAIL}
+    export PGADMIN_SETUP_PASSWORD=${PGADMIN_DEFAULT_PASSWORD}
+
+    # Initialize DB before starting Gunicorn
+    # Importing pgadmin4 (from this script) is enough
+    /venv/bin/python3 run_pgadmin.py
+
+    export PGADMIN_SERVER_JSON_FILE=${PGADMIN_SERVER_JSON_FILE:-/pgadmin4/servers.json}
+    # Pre-load any required servers
+    if [ -f "${PGADMIN_SERVER_JSON_FILE}" ]; then
+        # When running in Desktop mode, no user is created
+        # so we have to import servers anonymously
+        if [ "${PGADMIN_CONFIG_SERVER_MODE}" = "False" ]; then
+            /venv/bin/python3 /pgadmin4/setup.py --load-servers "${PGADMIN_SERVER_JSON_FILE}"
+        else
+            /venv/bin/python3 /pgadmin4/setup.py --load-servers "${PGADMIN_SERVER_JSON_FILE}" --user ${PGADMIN_DEFAULT_EMAIL}
+        fi
+    fi
+fi
+
+# Start Postfix to handle password resets etc.
+if [ -z ${PGADMIN_DISABLE_POSTFIX} ]; then
+    sudo /usr/sbin/postfix start
+fi
+
+# Get the session timeout from the pgAdmin config. We'll use this (in seconds)
+# to define the Gunicorn worker timeout
+TIMEOUT=$(cd /pgadmin4 && /venv/bin/python3 -c 'import config; print(config.SESSION_EXPIRATION_TIME * 60 * 60 * 24)')
+
+# NOTE: currently pgadmin can run only with 1 worker due to sessions implementation
+# Using --threads to have multi-threaded single-process worker
+
+if [ ! -z ${PGADMIN_ENABLE_TLS} ]; then
+    exec /venv/bin/gunicorn --timeout ${TIMEOUT} --bind ${PGADMIN_LISTEN_ADDRESS:-[::]}:${PGADMIN_LISTEN_PORT:-443} -w 1 --threads ${GUNICORN_THREADS:-25} --access-logfile ${GUNICORN_ACCESS_LOGFILE:--} --keyfile /certs/server.key --certfile /certs/server.cert -c gunicorn_config.py run_pgadmin:app
+else
+    exec /venv/bin/gunicorn --timeout ${TIMEOUT} --bind ${PGADMIN_LISTEN_ADDRESS:-[::]}:${PGADMIN_LISTEN_PORT:-80} -w 1 --threads ${GUNICORN_THREADS:-25} --access-logfile ${GUNICORN_ACCESS_LOGFILE:--} -c gunicorn_config.py run_pgadmin:app
+fi
+
